c21b28cf8df6f2d188e1e0b2c3816a410b0a9646
osutripathi <tripathisrujan.58@buckeyemail.osu.edu>
2023-01-20 19:20:24 -0500

    Srujan: Fixed bug in local host url

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from src.login import generate_login_portal as login_portal
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return login_portal("https://http://127.0.0.1:5000/")
+    return login_portal("https://127.0.0.1:5000/")
