8cd625f004d9cc134c650d0a20900ba6cbb7d847
Zach Jones <jones.6606@osu.edu>
2023-02-04 17:47:30 -0500

    Fixed linter issues

--- youtubeShortsTest/LikedShortsGetter.py
+++ youtubeShortsTest/LikedShortsGetter.py
@@ -4,10 +4,6 @@
 # See instructions for running these code samples locally:
 # https://developers.google.com/explorer-help/code-samples#python
 
-import os
-
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
 import googleapiclient.errors
 import requests
 import json
